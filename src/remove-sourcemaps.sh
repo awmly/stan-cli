db336a4c5d84aebb9543b69167f443d6336025e8
Andrew Womersley <a@wmly.uk>
2015-02-09 13:32:47 +0000

    Update reg exp in sed

--- src/remove-sourcemaps.sh
+++ src/remove-sourcemaps.sh
@@ -5,10 +5,7 @@ elif [ "$METHOD" = "remove-sourcemaps" ]; then
   SITE_ID=$( getConfigVar "SITE_ID" )
 
   # Remove css maps
-  sed -i.tmp 's/sourceMappingURL=.*\.map//g' httpdocs/cache/assets/${SITE_ID}-*.css
+  sed -i '' 's/sourceMappingURL=.*\.map//g' httpdocs/cache/assets/${SITE_ID}-*.css
 
   # Remove js maps
-  sed -i.tmp 's/\\/\\/# sourceMappingURL=.*\.map//g' httpdocs/cache/assets/${SITE_ID}-*.js
-
-  # Clean tmp files
-  rm -f httpdocs/cache/assets/*.tmp
+  sed -i '' 's/\/\/# sourceMappingURL=.*\.map//g' httpdocs/cache/assets/${SITE_ID}-*.js
