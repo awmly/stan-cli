4a47ee3f850bd1084ef3b5b56132afe9af61b2d6
Andrew Womersley <a@wmly.uk>
2015-02-09 13:06:42 +0000

    Get SITE_ID var

--- src/remove-sourcemaps.sh
+++ src/remove-sourcemaps.sh
@@ -1,6 +1,9 @@
 # Define remove-sourcemaps method
 elif [ "$METHOD" = "remove-sourcemaps" ]; then
 
+  # Get site ID
+  SITE_ID=$( getConfigVar "SITE_ID" )
+
   # Remove css maps
   sed -i.tmp 's/sourceMappingURL=.*\.map//g' httpdocs/cache/assets/${SITE_ID}-*.css
 
